14b2fe8372a68ac0e40c35cd9d64f23e930af11b
hector4213 <hectorpwns@gmail.com>
2021-03-25 14:35:25 -0400

    start character details page

--- src/components/CharacterDetails.jsx
+++ src/components/CharacterDetails.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const CharacterDetails = () => {
   const { charId } = useParams();
+
+  useEffect(() => {
+    console.log('mounted!');
+  }, []);
 
   return <div>{charId}</div>;
 };
