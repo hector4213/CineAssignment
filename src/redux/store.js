7dfc5a3b226848bf79a5de8fad60df6c1be588d5
hector4213 <hectorpwns@gmail.com>
2021-03-25 14:15:16 -0400

    introduce reducer to global state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import characterSlice from './ducks/characterSlice';
+import characterDetails from './ducks/charDetailsSlice';
 
 export default configureStore({
   reducer: {
     characters: characterSlice,
+    characterDetails,
   },
 });
