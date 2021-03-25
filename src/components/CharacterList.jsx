2dc18bab698c10f89eb9e3ea58cd2f08a43f9749
hector4213 <hectorpwns@gmail.com>
2021-03-24 23:26:28 -0400

    import state with useSelector and dispatch

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCharacters } from '../redux/ducks/characterSlice';
+
 const CharacterList = () => {
-  return <div>Im a CharacterCard</div>;
+  const dispatch = useDispatch();
+  const { results, isLoading, isError } = useSelector(
+    (state) => state.characters,
+  );
+
+  useEffect(() => {
+    dispatch(fetchCharacters());
+    console.log({ results, isLoading, isError });
+  }, []);
+
+  return <div>Im a CharacterList</div>;
 };
 
 export default CharacterList;
