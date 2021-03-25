51699996866669d89ea071f6f85197fbb286248c
hector4213 <hectorpwns@gmail.com>
2021-03-25 15:05:22 -0400

    use redux state, use params id as ad parameter for API call

--- src/components/CharacterDetails.jsx
+++ src/components/CharacterDetails.jsx
@@ -1,14 +1,33 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getCharById, clearCharacter } from '../redux/ducks/charDetailsSlice';
 
 const CharacterDetails = () => {
   const { charId } = useParams();
+  const dispatch = useDispatch();
+  const { isLoading, char, isError } = useSelector(
+    (state) => state.characterDetails,
+  );
 
   useEffect(() => {
-    console.log('mounted!');
+    if (Object.keys(char).length === 0) {
+      dispatch(getCharById(charId));
+    }
+    console.log(isLoading, isError);
+
+    return () => {
+      dispatch(clearCharacter());
+    };
   }, []);
 
-  return <div>{charId}</div>;
+  const { name } = char;
+
+  return (
+    <div>
+      {charId} {name}
+    </div>
+  );
 };
 
 export default CharacterDetails;
