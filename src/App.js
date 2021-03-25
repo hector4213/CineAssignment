e20dc8cb6dcaa00e641f99516a4968513174f9bf
hector4213 <hectorpwns@gmail.com>
2021-03-25 13:52:44 -0400

    use react router for routing, create route to characters page with parameters, move state up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useEffect } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCharacters } from './redux/ducks/characterSlice';
 import CharacterList from './components/CharacterList';
+import CharacterDetails from './components/CharacterDetails';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  const { results, isLoading, isError } = useSelector(
+    (state) => state.characters,
+  );
+  useEffect(() => {
+    if (results.length < 1) {
+      dispatch(fetchCharacters());
+    }
+    console.log({ results, isLoading, isError });
+  }, []);
   return (
     <div className="App">
-      <CharacterList />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <CharacterList charData={results} />
+          </Route>
+          <Route path="/characters/:charId">
+            <CharacterDetails />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
