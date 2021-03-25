b2847811186dd409e6b1357d0fe6c47c4dac5e01
hector4213 <hectorpwns@gmail.com>
2021-03-24 22:40:20 -0400

    add place holder components

---
+++ src/components/CharacterCard.jsx
@@ -0,0 +1,5 @@
+const CharacterCard = () => {
+  return <div>Im going to be a CharacterCard</div>;
+};
+
+export default CharacterCard;
---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,5 @@
+const CharacterList = () => {
+  return <div>Im a CharacterCard</div>;
+};
+
+export default CharacterList;
