a5259338bfced7408e0d3fbe658d3f3748729e9b
hector4213 <hectorpwns@gmail.com>
2021-03-26 12:40:29 -0400

    write reducer, state will keep track of prev and next page on each page change

--- src/redux/ducks/characterSlice.js
+++ src/redux/ducks/characterSlice.js
@@ -9,12 +9,23 @@ export const fetchCharacters = createAsyncThunk(
   },
 );
 
+export const goToPage = createAsyncThunk(
+  'pages/goToPage',
+  async (prevOrNext) => {
+    const response = await SWAPI.goToPage(prevOrNext);
+    return response;
+  },
+);
+
 const characterSlice = createSlice({
   name: 'characters',
   initialState: {
     isLoading: true,
     results: [],
     isError: false,
+    page: 1,
+    next: null,
+    prev: null,
   },
   reducers: {},
   extraReducers: {
@@ -27,7 +38,15 @@ const characterSlice = createSlice({
     [fetchCharacters.fulfilled]: (state, { payload }) => {
       const characters = payload.results.map((char) => char);
       state.results = characters;
+      state.next = payload.next;
+      state.prev = payload.previous;
       state.isLoading = false;
+    },
+    [goToPage.fulfilled]: (state, { payload }) => {
+      const newPage = payload.results.map((char) => char);
+      state.results = newPage;
+      state.next = payload.next;
+      state.prev = payload.previous;
     },
   },
 });
