47decd4777fc02b2158163fe7b4ffb26a746a84d
hector4213 <hectorpwns@gmail.com>
2021-03-26 12:39:37 -0400

    add api call for going to next page

--- src/services/SWAPI.js
+++ src/services/SWAPI.js
@@ -12,7 +12,16 @@ const getPersonById = async (id) => {
   return data;
 };
 
+// NOTE: Response from resources i.e /people return a url string to indicate if next or prev page is present
+
+const goToPage = async (prevOrNext) => {
+  const response = await fetch(prevOrNext);
+  const nextOrPrevPage = response.json();
+  return nextOrPrevPage;
+};
+
 export default {
   getPeople,
   getPersonById,
+  goToPage,
 };
