520d35d970d50de05b32fbb63387af485ef74973
hector4213 <hectorpwns@gmail.com>
2021-03-27 23:16:33 -0400

    add await to .json() on api calls

--- src/services/SWAPI.js
+++ src/services/SWAPI.js
@@ -2,19 +2,19 @@ const baseURL = 'https://swapi.dev/api/people';
 
 const getPeople = async () => {
   const response = await fetch(baseURL);
-  const data = response.json();
+  const data = await response.json();
   return data;
 };
 
 const getPersonById = async (id) => {
   const response = await fetch(`${baseURL}/${id}`);
-  const data = response.json();
+  const data = await response.json();
   return data;
 };
 
 const goToPage = async (prevOrNext) => {
   const response = await fetch(prevOrNext);
-  const nextOrPrevPage = response.json();
+  const nextOrPrevPage = await response.json();
   return nextOrPrevPage;
 };
 
