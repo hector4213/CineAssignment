18006b7fa0a043d50878b9df1fa43d8cf07b4fe0
hector4213 <hectorpwns@gmail.com>
2021-03-28 00:25:19 -0400

    style list

--- src/components/CharacterListItem.jsx
+++ src/components/CharacterListItem.jsx
@@ -1,13 +1,27 @@
 import PropTypes from 'prop-types';
+import R2D2 from '../assets/r2-d2.png';
 
 const CharacterListItem = ({ character }) => {
   const { name, birth_year, height, mass } = character;
   return (
-    <li>
-      name:{name}
-      BirthYear{birth_year}
-      height{height}
-      mass{mass}
+    <li className="border-gray-400 flex flex-row mb-2">
+      <div className="transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white dark:bg-gray-800 rounded-md flex flex-1 items-center p-4">
+        <img className="w-10 h-10" src={R2D2} alt="char select logo" />
+        <div className="flex  justify-evenly ">
+          <div className="text-sm p-2">
+            <span className="font-medium">Name:</span> {name}
+          </div>
+          <div className="text-sm p-2">
+            <span className="font-medium">Birth year:</span> {birth_year}
+          </div>
+          <div className="text-sm p-2">
+            <span className="font-medium">Height:</span> {height}
+          </div>
+          <div className="text-sm p-2">
+            <span className="font-medium">Mass:</span> {mass}
+          </div>
+        </div>
+      </div>
     </li>
   );
 };
