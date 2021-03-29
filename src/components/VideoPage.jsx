e35b2772be7f4fa30a9774c0c1917921052fb792
hector4213 <hectorpwns@gmail.com>
2021-03-29 00:53:34 -0400

    add video page

---
+++ src/components/VideoPage.jsx
@@ -0,0 +1,23 @@
+import { useHistory } from 'react-router-dom';
+import Video from './Video';
+
+const VideoPage = () => {
+  const history = useHistory();
+  const goToApp = () => {
+    history.push('/characters');
+  };
+  return (
+    <div className="min-w-screen min-h-screen p-4 bg-black relative">
+      <Video />
+      <button
+        onClick={goToApp}
+        className="absolute z-50 top-10 left-10 py-2 px-4 flex-shrink-0 bg-white focus:outline-none hover:bg-gray-200 text-black transition ease-in duration-200 text-center text-base font-semibold rounded-lg"
+        type="button"
+      >
+        GO TO APP
+      </button>
+    </div>
+  );
+};
+
+export default VideoPage;
