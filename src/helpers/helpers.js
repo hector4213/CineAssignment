e87e5a738f7c2e1140f9f3ac5ee5ac81889fc8ff
hector4213 <hectorpwns@gmail.com>
2021-03-28 23:38:16 -0400

    function to get curr page

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -3,7 +3,17 @@ export const getIDFromUrl = (charURL) => {
   const test = charURL.match(regex);
   return test[1];
 };
+export const getPageNum = (nextPage, prevPage) => {
+  const regex = /(\d+)/;
+  if (nextPage === null) {
+    const test = prevPage.match(regex);
+    return Number(test[0]) + 1;
+  }
+  const test = nextPage.match(regex);
+  return test[0] - 1;
+};
 
 export default {
   getIDFromUrl,
+  getPageNum,
 };
