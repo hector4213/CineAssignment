ab435e7a6ab14046cd4df1697f982cb3e45bedb3
hector4213 <hectorpwns@gmail.com>
2021-03-29 00:54:30 -0400

    add video and component (external lib)

---
+++ src/components/Video.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ReactVideo } from 'reactjs-media';
+import Intro from '../assets/intro.mp4';
+import Poster from '../assets/poster.jpg';
+
+const Video = () => {
+  return (
+    <div>
+      <ReactVideo src={Intro} poster={Poster} primaryColor="black" />
+    </div>
+  );
+};
+
+export default Video;
